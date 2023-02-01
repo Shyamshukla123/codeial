8c975919b3deaaead43bad2c66c79b3b9535b64d
Shyamshukla123 <shyamshukla789@gmail.com>
2023-02-01 23:22:30 +0530

    adding user roote for profile controlller

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port = 8000;
 
+// express use routes
+
+app.use('/', require('./routes/index'));
+
+
+
 app.listen(port, (err) => {
     if (err) {
         console.log(`'Erro in server : ${err}`);
